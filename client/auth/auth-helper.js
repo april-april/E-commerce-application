821d4d0cb283c5b573854eddc12cfb703adf0b86
Vincent L <vincentluoster93@gmail.com>
2018-08-12 04:04:04 -0700

    update user

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -3,27 +3,37 @@ import { signout } from './api-auth.js'
 // retrieve JWT credentials from client-side sessionStorage
 const auth = {
   isAuthenticated() {
-    if (typeof window == "undefined")
-      return false
+		if (typeof window == "undefined")
+			return false
 
-    if (sessionStorage.getItem('jwt'))
-      return JSON.parse(sessionStorage.getItem('jwt'))
-    else
-      return false
+		if (sessionStorage.getItem('jwt'))
+			return JSON.parse(sessionStorage.getItem('jwt'))
+		else
+			return false
   },
   authenticate(jwt, cb) {
-    if (typeof window !== "undefined")
-      sessionStorage.setItem('jwt', JSON.stringify(jwt))
-    cb()
+		if (typeof window !== "undefined")
+			sessionStorage.setItem('jwt', JSON.stringify(jwt))
+		cb()
   },
   signout(cb) {
-    if (typeof window !== "undefined")
-      sessionStorage.removeItem('jwt')
-    cb()
-    //optional
-    signout().then((data) => {
-      document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-    })
+		if (typeof window !== "undefined")
+			sessionStorage.removeItem('jwt')
+		cb()
+		//optional
+		signout().then((data) => {
+			document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+	})
+  },
+  updateUser(user, cb) {
+    if(typeof window !== "undefined"){
+		if(sessionStorage.getItem('jwt')){
+			let auth = JSON.parse(sessionStorage.getItem('jwt'))
+			auth.user = user
+			sessionStorage.setItem('jwt', JSON.stringify(auth))
+			cb()
+      	}
+    }
   }
 }
 
