df98d628c53e4bcc370755b87d5264b806c04729
Vincent L <vincentluoster93@gmail.com>
2018-08-13 18:32:20 -0700

    shop

--- server/controllers/shop.controller.js
+++ server/controllers/shop.controller.js
@@ -1,14 +1,26 @@
-import Shop from '../models/shop.model'
-import _ from 'lodash'
 
 const create = (req, res, next) => {
     let form = new formidable.IncomingForm()
-      form.keepExtensions = true
-      form.parse(req, (err, fields, files) => {
-      if (err) {
-          res.status(400).json({
-              message: "Image could not be uploaded"
-          })
+        form.keepExtensions = true
+        form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.status(400).json({
+                message: "Image could not be uploaded"
+            })
         }
+        let shop = new Shop(fields)
+        shop.owner= req.profile
+        if(files.image){
+            shop.image.data = fs.readFileSync(files.image.path)
+            shop.image.contentType = files.image.type
+        }
+        shop.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+                })
+            }
+            res.status(200).json(result)
+        })
     })
 }
