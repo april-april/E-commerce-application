b229155d4ecd2f177576202a84f7b3c278cb7561
Vincent L <vincentluoster93@gmail.com>
2018-08-15 23:10:39 -0700

    myshops component

---
+++ client/shop/MyShops.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+const styles = theme => ({
+
+})
+
+class MyShops extends Component {
+    state = {
+        shops:[],
+        redirectToSignin: false
+    }
+
+}
+
+export default withStyles(styles)(MyShops)
