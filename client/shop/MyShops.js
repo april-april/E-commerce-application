da57af851fa95a508447202402700fcf8f7d7797
Vincent L <vincentluoster93@gmail.com>
2018-08-15 23:23:01 -0700

    load shops

--- client/shop/MyShops.js
+++ client/shop/MyShops.js
@@ -1,16 +1,58 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Redirect, Link } from 'react-router-dom'
 
 const styles = theme => ({
 
 })
 
 class MyShops extends Component {
+    
     state = {
         shops:[],
         redirectToSignin: false
     }
 
+    loadShops = () => {
+        const jwt = auth.isAuthenticated()
+        listByOwner({
+            userId: jwt.user._id
+        }, {t: jwt.token}).then((data) => {
+            if (data.error) {
+                this.setState({redirectToSignin: true})
+            } else {
+                this.setState({shops: data})
+            }
+        })
+    }
+
+    removeShop = (shop) => {
+        const updatedShops = this.state.shops
+        const index = updatedShops.indexOf(shop)
+        updatedShops.splice(index, 1)
+        this.setState({shops: updatedShops})
+    }
+    componentDidMount = () => {
+        this.loadShops()
+    }
+    render() {
+        const { classes } = this.props
+        const redirectToSignin = this.state.redirectToSignin
+        if (redirectToSignin) {
+            return <Redirect to='/signin'/>
+        }
+        return (
+        <div>
+            <Paper className={classes.root} elevation={4}>
+
+            </Paper>
+        </div>
+        )
+    }
 }
+
+MyShops.propTypes = {
+    classes: PropTypes.object.isRequired
+  }
 
 export default withStyles(styles)(MyShops)
