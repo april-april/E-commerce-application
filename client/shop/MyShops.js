785f2a1fd1f288653e0605f0b9816580f5502cf0
Vincent L <vincentluoster93@gmail.com>
2018-08-16 20:57:28 -0700

    myshop component update

--- client/shop/MyShops.js
+++ client/shop/MyShops.js
@@ -54,6 +54,33 @@ class MyShops extends Component {
                     </Link>
                 </span>
             </Typography>
+            <List dense>
+            {this.state.shops.map((shop, i) => {
+                return   <span key={i}>
+                    <ListItem button>
+                        <ListItemAvatar>
+                            <Avatar src={'/api/shops/logo/'+shop._id+"?" + new Date().getTime()}/>
+                        </ListItemAvatar>
+                        <ListItemText primary={shop.name} secondary={shop.description}/>
+                        { auth.isAuthenticated().user && auth.isAuthenticated().user._id == shop.owner._id &&
+                            (<ListItemSecondaryAction>
+                                <Link to={"/seller/orders/" + shop.name+ '/'+shop._id}>
+                                    <Button aria-label="Orders" color="primary">
+                                        View Orders
+                                    </Button>
+                                </Link>
+                                <Link to={"/seller/shop/edit/" + shop._id}>
+                                    <IconButton aria-label="Edit" color="primary">
+                                        <Edit/>
+                                    </IconButton>
+                                </Link>
+                                <DeleteShop shop={shop} onRemove={this.removeShop}/>
+                            </ListItemSecondaryAction>)
+                        }
+                    </ListItem>
+                    <Divider/>
+                </span>})}
+            </List>
 
             </Paper>
         </div>
