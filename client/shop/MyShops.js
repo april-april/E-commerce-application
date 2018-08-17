e89ee847ba3facb42f2fc59e0c35a7820359c816
Vincent L <vincentluoster93@gmail.com>
2018-08-16 20:57:44 -0700

    import

--- client/shop/MyShops.js
+++ client/shop/MyShops.js
@@ -1,6 +1,19 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import { Redirect, Link } from 'react-router-dom'
+import {withStyles} from 'material-ui/styles'
+import Paper from 'material-ui/Paper'
+import List, {ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText} from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import IconButton from 'material-ui/IconButton'
+import Icon from 'material-ui/Icon'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
+import Edit from 'material-ui-icons/Edit'
+import Divider from 'material-ui/Divider'
+import auth from './../auth/auth-helper'
+import {listByOwner} from './api-shop.js'
+import {Redirect, Link} from 'react-router-dom'
+import DeleteShop from './DeleteShop'
 
 const styles = theme => ({
 
