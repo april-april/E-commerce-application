a2e8b87ac6c943ff3d5a26ef655da9093aae5e98
Vincent L <vincentluoster93@gmail.com>
2018-08-18 22:44:48 -0700

    shop render

--- client/shop/Shop.js
+++ client/shop/Shop.js
@@ -22,7 +22,7 @@ class Shop extends Component {
             if (data.error) {
                 this.setState({error: data.error})
             } else {
-                this.setState({})
+                this.setState({products: data})
             }
         })
     }
@@ -41,16 +41,24 @@ class Shop extends Component {
     }
 
     render() {
-        <CardContent>
-            <Typography type="headline" component="h2" className={classes.title}>
-                {this.state.shop.name}
-            </Typography>
-            <br/>
-            <Avatar src={logoUrl} className={classes.bigAvatar}/><br/>
-                <Typography type="subheading" component="h2" className={classes.subheading}>
-                    {this.state.shop.description}
-                </Typography><br/>
-        </CardContent>
+        const logoUrl = this.state.shop._id
+            ? `/api/shops/logo/${this.state.shop._id}?${new Date().getTime()}`
+            : '/api/shops/defaultphoto'
+
+        const {classes} = this.props
+
+        return(<div>
+            <CardContent>
+                <Typography type="headline" component="h2" className={classes.title}>
+                    {this.state.shop.name}
+                </Typography>
+                <br/>
+                <Avatar src={logoUrl} className={classes.bigAvatar}/><br/>
+                    <Typography type="subheading" component="h2" className={classes.subheading}>
+                        {this.state.shop.description}
+                    </Typography><br/>
+            </CardContent>
+        </div>)
     }
 }
 
