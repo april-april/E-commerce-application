562d2616e37fceba5da1488ce5387dd9c2485fbd
Vincent L <vincentluoster93@gmail.com>
2018-08-22 23:39:31 -0700

    product model

---
+++ server/models/product.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  image: {
+    data: Buffer,
+    contentType: String
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  category: {
+    type: String
+  },
+})
+
+export default mongoose.model('Product', ProductSchema)
