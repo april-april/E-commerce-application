851b018304a5a96e25f51d3ff21af5bd9f54f4fe
Vincent L <vincentluoster93@gmail.com>
2018-08-22 23:08:26 -0700

    fix bug (woops lols)

--- server/controllers/shop.controller.js
+++ server/controllers/shop.controller.js
@@ -111,20 +111,6 @@ const remove = (req, res, next) => {
     })
 }
 
-const remove = (params, credentials) => {
-    return fetch('/api/shops/' + params.shopId, {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + credentials.t
-        }
-    }).then((response) => {
-        return response.json()
-    }).catch((err) => {
-        console.log(err)
-    })
-}
 
 export default {
     create,
