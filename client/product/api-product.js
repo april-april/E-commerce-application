c3d65bdb8efde0b959437d1f29c21f65297b09b5
Vincent L <vincentluoster93@gmail.com>
2018-08-23 03:27:23 -0700

    fetch create API

---
+++ client/product/api-product.js
@@ -0,0 +1,13 @@
+const create = (params, credentials, product) => {
+    return fetch('/api/products/by/'+ params.shopId, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + credentials.t
+        },
+        body: product
+        })
+        .then((response) => {
+            return response.json()
+        }).catch((err) => console.log(err))
+}
