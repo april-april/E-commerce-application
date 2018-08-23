e2106d37e6d7eeac1ff90aa8a4cdf17bf412accd
Vincent L <vincentluoster93@gmail.com>
2018-08-23 03:14:23 -0700

    bug fixes and product controller

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,23 @@
+import Product from '../models/product.model'
+import _ from 'lodash'
+import errorHandler from './../helpers/dbErrorHandler'
+import formidable from 'formidable'
+import fs from 'fs'
+
+const create = (req, res, next) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                message: "Image could not be uploaded"
+            })
+        }
+        let product = new Product(fields)
+        product.shop= req.shop
+        if(files.image){
+            product.image.data = fs.readFileSync(files.image.path)
+            product.image.contentType = files.image.type
+        }
+    })
+}
