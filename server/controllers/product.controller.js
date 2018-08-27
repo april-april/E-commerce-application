a69bc1ca4675c2a78545635db2a79587b179b1c1
Vincent L <vincentluoster93@gmail.com>
2018-08-26 23:30:12 -0700

    listbyshop controller

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -30,6 +30,18 @@ const create = (req, res, next) => {
     })
 }
 
+const listByShop = (req, res) => {
+    Product.find({shop: req.shop._id}, (err, products) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        res.json(products)
+    }).populate('shop', '_id name').select('-image')
+}
+
 export default {
-    create
+    create,
+    listByShop
 }
