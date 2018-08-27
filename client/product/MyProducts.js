c749fdf183496f93ef04727c6d3fa8ee4a5c9955
Vincent L <vincentluoster93@gmail.com>
2018-08-26 23:58:32 -0700

    myproducts component render

--- client/product/MyProducts.js
+++ client/product/MyProducts.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
+import Button from 'material-ui/Button'
+import Card, {CardMedia} from 'material-ui/Card'
+import IconButton from 'material-ui/IconButton'
+import Edit from 'material-ui-icons/Edit'
+import Icon from 'material-ui/Icon'
+import List, {ListItem, ListItemSecondaryAction} from 'material-ui/List'
+import Typography from 'material-ui/Typography'
+import {Link} from 'react-router-dom'
+import Divider from 'material-ui/Divider'
+import auth from './../auth/auth-helper'
 import {listByShop} from './../product/api-product.js'
-
+import DeleteProduct from './../product/DeleteProduct'
 
 const styles = theme => ({
 
@@ -38,7 +48,45 @@ class MyProducts extends Component {
 
   render() {
     const {classes} = this.props
+    return (
+      <Card className={classes.products}>
+        <List dense>
+        {this.state.products.map((product, i) => {
+            return <span key={i}>
+              <ListItem>
+                <CardMedia
+                  className={classes.cover}
+                  image={'/api/product/image/'+product._id+"?" + new Date().getTime()}
+                  title={product.name}
+                />
+                <div className={classes.details}>
+                  <Typography type="headline" component="h2" color="primary" className={classes.productTitle}>
+                    {product.name}
+                  </Typography>
+                  <Typography type="subheading" component="h4" className={classes.subheading}>
+                    Quantity: {product.quantity} | Price: ${product.price}
+                  </Typography>
+                </div>
+                <ListItemSecondaryAction>
+                  <Link to={"/seller/"+product.shop._id+"/"+product._id+"/edit"}>
+                    <IconButton aria-label="Edit" color="primary">
+                      <Edit/>
+                    </IconButton>
+                  </Link>
+                  <DeleteProduct
+                    product={product}
+                    shopId={this.props.shopId}
+                    onRemove={this.removeProduct}/>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider/></span>})}
+        </List>
+      </Card>)
   }
+}
+MyProducts.propTypes = {
+  classes: PropTypes.object.isRequired,
+  shopId: PropTypes.string.isRequired
 }
 
 export default withStyles(styles)(MyProducts)
