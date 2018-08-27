0e713c1b1ea452e8f51225dc3201b0aca1af6f96
Vincent L <vincentluoster93@gmail.com>
2018-08-26 23:27:13 -0700

    new product route

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -7,7 +7,8 @@ const router = express.Router()
 
 router.route('/api/products/by/:shopId')
     .post(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.create)
-
+    .get(productCtrl.listByShop)
+    
 router.param('shopId', shopCtrl.shopByID)
 
 export default router
