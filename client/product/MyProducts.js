d2ffdfd0b36d446b25c0f8da7336b741c72086c4
Vincent L <vincentluoster93@gmail.com>
2018-08-27 23:09:24 -0700

    myproduct styles

--- client/product/MyProducts.js
+++ client/product/MyProducts.js
@@ -15,6 +15,32 @@ import {listByShop} from './../product/api-product.js'
 import DeleteProduct from './../product/DeleteProduct'
 
 const styles = theme => ({
+	products: {
+		padding: '24px'
+	},
+	addButton:{
+		float:'right'
+	},
+	leftIcon: {
+		marginRight: "8px"
+	},
+	title: {
+		margin: theme.spacing.unit * 2,
+		color: theme.palette.protectedTitle,
+		fontSize: '1.2em'
+	},
+	subheading: {
+		marginTop: theme.spacing.unit * 2,
+		color: theme.palette.openTitle
+	},
+	cover: {
+		width: 110,
+		height: 100,
+		margin: '8px'
+	},
+	details: {
+		padding: '10px'
+	},
 
 })
 
@@ -46,53 +72,53 @@ class MyProducts extends Component {
 		this.setState({shops: updatedProducts})
 	}
 
-  render() {
-    const {classes} = this.props
-    return (
-      <Card className={classes.products}>
-        <Typography type="title" className={classes.title}>
-          Products
-          <span className={classes.addButton}>
-            <Link to={"/seller/"+this.props.shopId+"/products/new"}>
+	render() {
+		const {classes} = this.props
+		return (
+			<Card className={classes.products}>
+				<Typography type="title" className={classes.title}>
+					Products
+					<span className={classes.addButton}>
+						<Link to={"/seller/"+this.props.shopId+"/products/new"}>
 				<Button color="primary" variant="raised">
 					<Icon className={classes.leftIcon}>add_box</Icon>  New Product
 				</Button>
-            </Link>
-          </span>
-        </Typography>
-        <List dense>
-        {this.state.products.map((product, i) => {
-            return <span key={i}>
-              <ListItem>
-                <CardMedia
-                  className={classes.cover}
-                  image={'/api/product/image/'+product._id+"?" + new Date().getTime()}
-                  title={product.name}
-                />
-                <div className={classes.details}>
-                  <Typography type="headline" component="h2" color="primary" className={classes.productTitle}>
-                    {product.name}
-                  </Typography>
-                  <Typography type="subheading" component="h4" className={classes.subheading}>
-                    Quantity: {product.quantity} | Price: ${product.price}
-                  </Typography>
-                </div>
-                <ListItemSecondaryAction>
-                  <Link to={"/seller/"+product.shop._id+"/"+product._id+"/edit"}>
-                    <IconButton aria-label="Edit" color="primary">
-                      	<Edit/>
-                    </IconButton>
-                  </Link>
-                  <DeleteProduct
-                    product={product}
-                    shopId={this.props.shopId}
-                    onRemove={this.removeProduct}/>
-                </ListItemSecondaryAction>
-              </ListItem>
-              <Divider/></span>})}
-        </List>
-      </Card>)
-  }
+						</Link>
+					</span>
+				</Typography>
+				<List dense>
+				{this.state.products.map((product, i) => {
+						return <span key={i}>
+							<ListItem>
+								<CardMedia
+									className={classes.cover}
+									image={'/api/product/image/'+product._id+"?" + new Date().getTime()}
+									title={product.name}
+								/>
+								<div className={classes.details}>
+									<Typography type="headline" component="h2" color="primary" className={classes.productTitle}>
+										{product.name}
+									</Typography>
+									<Typography type="subheading" component="h4" className={classes.subheading}>
+										Quantity: {product.quantity} | Price: ${product.price}
+									</Typography>
+								</div>
+								<ListItemSecondaryAction>
+									<Link to={"/seller/"+product.shop._id+"/"+product._id+"/edit"}>
+										<IconButton aria-label="Edit" color="primary">
+												<Edit/>
+										</IconButton>
+									</Link>
+									<DeleteProduct
+										product={product}
+										shopId={this.props.shopId}
+										onRemove={this.removeProduct}/>
+								</ListItemSecondaryAction>
+							</ListItem>
+							<Divider/></span>})}
+				</List>
+			</Card>)
+	}
 }
 MyProducts.propTypes = {
 	classes: PropTypes.object.isRequired,
