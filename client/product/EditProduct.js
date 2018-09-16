24704fde99df2280b6fcebf4c0b49b021dea79bf
Vincent L <vincentluoster93@gmail.com>
2018-09-16 16:27:45 -0700

    component

--- client/product/EditProduct.js
+++ client/product/EditProduct.js
@@ -69,6 +69,20 @@ class EditProduct extends Component {
 
 	render() {
 
+		const {classes} = this.props
+		return (<div>
+			<Card className={classes.card}>
+				<CardContent>
+					<Typography type="headline" component="h2" className={classes.title}>
+						Edit Product
+					</Typography><br/>
+					<Avatar src={imageUrl} className={classes.bigAvatar}/><br/>
+					<input accept="image/*" onChange={this.handleChange('image')} className={classes.input} id="icon-button-file" type="file" />
+
+				</CardContent>
+
+			</Card>
+		</div>)
 	}
 }
 
