1b3d6fd11ac5de0423b27c01e6629b9f81a0d4fa
Vincent L <vincentluoster93@gmail.com>
2018-09-22 01:30:27 -0700

    delete product component

---
+++ client/product/DeleteProduct.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import auth from './../auth/auth-helper'
+import {remove} from './api-product.js'
+
+class DeleteProduct extends Component {
+	state = {
+		open: false
+	}
+	deleteProduct = () => {
+		const jwt = auth.isAuthenticated()
+		remove({
+			shopId: this.props.shopId,
+			productId: this.props.product._id
+		}, {t: jwt.token}).then((data) => {
+			if (data.error) {
+				console.log(data.error)
+			} else {
+				this.setState({open: false}, () => {
+					this.props.onRemove(this.props.product)
+				})
+			}
+		})
+	}
+
+	render() {
+	
+	}
+}
+DeleteProduct.propTypes = {
+	shopId: PropTypes.string.isRequired,
+}
+export default DeleteProduct
